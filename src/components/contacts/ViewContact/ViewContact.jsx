37fd09d019b44742ef31a4ee122c7723c58514e4
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-08-27 16:31:31 +1000

    styling view contyact page

--- src/components/contacts/ViewContact/ViewContact.jsx
+++ src/components/contacts/ViewContact/ViewContact.jsx
@@ -4,7 +4,14 @@ let ViewContact = () => {
     return (
         <React.Fragment>
             <section className="view-contact">
-                
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
+                            <p className="h3 text-warning">View Contact</p>
+                            <p className="fst-italic">Contact Details</p>
+                        </div>
+                    </div>
+                </div>
             </section>
         </React.Fragment>
     )
