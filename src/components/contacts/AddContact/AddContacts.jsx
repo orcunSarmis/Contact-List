51ed112f0f08f003e13cbc5fed116d9bce17477c
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-08-27 15:55:44 +1000

    working on add contatct

--- src/components/contacts/AddContact/AddContacts.jsx
+++ src/components/contacts/AddContact/AddContacts.jsx
@@ -3,7 +3,28 @@ import React from "react";
 let AddContact = () => {
     return (
         <React.Fragment>
-            <h2>AddContact</h2>
+            <section className="add-contact p-3">
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
+                            <p className="h4 text-success fw-bold">Create Contact</p>
+                            <p className="fst-italic">Your Contact</p>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-2">
+                            <form>
+                                <div className="mb-2">
+                                    <input type="text" className="form-control" placeholder="Name"/>
+                                </div>
+                                <div className="mb-2">
+                                    <input type="text" className="form-control" placeholder="Mobile"/>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </React.Fragment>
     )
 };
