1cfd533456aafb87939322c4f1c982a0687eac02
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-08-27 16:57:24 +1000

    row added

--- src/components/contacts/ContactList/ContactList.jsx
+++ src/components/contacts/ContactList/ContactList.jsx
@@ -39,6 +39,7 @@ let ContacList = () => {
 
             <section className="contact-list">
                 <div className="container">
+                    <div className="row">
                     <div className="col-md-6">
                         <div className="card">
                             <div className="card-body">
@@ -69,7 +70,40 @@ let ContacList = () => {
                                 </div>
                                  </div>
                             </div>
-                        </div>
+                        </div>                       
+                    </div>
+                    <div className="col-md-6">
+                        <div className="card">
+                            <div className="card-body">
+                                 <div className="row align-items-center d-flex justify-content-around">
+                                 <div className="col-md-4">
+                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaMLsn5xs11l_ijJwV8CiP9Dt4yaXatrWcgw&usqp=CAU" className="img-fluid contact-img"/>
+                                </div>
+                                <div className="col-md-7">
+                                    <ul className="list-group">
+                                        <li className="list-group-item list-group-item-action">
+                                            Name: <span className="fw-bold">Claude</span>
+                                        </li>
+                                        <li className="list-group-item list-group-item-action">
+                                            Mobile: <span className="fw-bold">0499258461</span>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div className="col-md-1 d-flex flex-column align-items-center">
+                                    <Link to={'/contacts/view/:contactId'} className="btn btn-warning my-1">
+                                        <i className="fa fa-eye"/>
+                                    </Link>
+                                    <Link to={'/contacts/edit/:contactId'} className="btn btn-primary my-1">
+                                        <i className="fa fa-pen"/>
+                                    </Link>
+                                    <button className="btn btn-danger my-1">
+                                        <i className="fa fa-trash"/>
+                                    </button>
+                                </div>
+                                 </div>
+                            </div>
+                        </div>                      
+                    </div>
                     </div>
                 </div>
             </section>
